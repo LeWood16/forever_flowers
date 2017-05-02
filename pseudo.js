fd984f2ab274636fd7b3f94c0f08b6f737c63b4a
Luke Wood <lewoodwebdev@gmail.com>
2017-05-02 00:37:41 +0000

    finished about-us outline, added first of product icons

---
+++ pseudo.js
@@ -0,0 +1,14 @@
+/*
+
+-create IA, then use that to build site's design
+    - include everything you want in the site in the IA
+    -
+
+-create logo
+-run "Debbie's Den" by mum, Forever Flowers is pretty taken
+
+-look into URLs, then head over to Cloud Flare for the 
+security stuff
+
+
+*/
