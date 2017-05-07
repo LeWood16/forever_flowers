585516fd36da29ed9899159d21fb7de989c7e0b6
Luke Wood <lewoodwebdev@gmail.com>
2017-05-07 01:16:52 +0000

    product icons added

--- pseudo.js
+++ pseudo.js
@@ -1,5 +1,19 @@
 /*
 
+
+-make site responsive (make 'about' and 'products' sections 
+scale larger at larger screensizes like top banner does)
+    -start at mobile, then establish breakpoints
+    
+-make icons for products
+    -flowers
+    -hats
+    -christmas decorations
+
+-set up form element for emailing myself
+
+
+
 -create IA, then use that to build site's design
     - include everything you want in the site in the IA
     -
